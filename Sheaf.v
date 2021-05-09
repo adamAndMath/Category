caed4d175dfeeb4dbf0b404e66290d70ed203b55
Adam <adam.and.math@gmail.com>
2021-05-09 05:15:19 +0200

    small products of sheafs

--- Sheaf.v
+++ Sheaf.v
@@ -5,15 +5,16 @@ Structure Sheaf (T: Topology) (C: SProdCategory) := {
   smap {X Y: Topology.Open T}: Y ~> X -> sobj X ~> sobj Y;
   smap_id (X: Topology.Open T): smap (id X) = id (sobj X);
   smap_comp {Z Y X: Topology.Open T} (g: X ~> Y) (f: Y ~> Z): smap (f ∘ g) = smap g ∘ smap f;
-  smap_cover {I} (f: I -> Topology.Open T) (e: sobj (scoprod f) ~> ∏ i, sobj (f i)):
-    ∏' i, smap (ι i) = e <-> ((∏) i, ∏' j, smap π₁) ∘ e = (∏' i, (∏) j, smap π₂) ∘ e;
+  smap_cover {I} {X} {f: I -> Topology.Open T} (e: X ~> ∏ i, sobj (f i)):
+    (forall i j, smap π₁ ∘ @π _ I (fun i => sobj (f i)) i ∘ e = smap π₂ ∘ @π _ I (fun i => sobj (f i)) j ∘ e) ->
+    exists! u: X ~> sobj (scoprod f), ∏' i, smap (ι i) ∘ u = e;
 }.
 
 Arguments sobj {T C} s t.
 Arguments smap {T C} s {X Y} f.
 Arguments smap_id {T C} s X.
 Arguments smap_comp {T C} s {Z Y X} g f.
-Arguments smap_cover {T C} s {I} f e.
+Arguments smap_cover {T C} s {I X f} e.
 
 Coercion sobj: Sheaf >-> Funclass.
 
@@ -76,6 +77,101 @@ Next Obligation.
 Qed.
 
 Canonical cat (T: Topology) (C: SProdCategory) := Category.Pack (Sheaf T C) (cat_mixin T C).
+
+Program Definition sprodCat_mixin (T: Topology) (C: SProdCategory) := SProdCategory.Mixin (cat T C)
+  (fun I F => {|
+    sobj X := ∏ i, F i X;
+    smap X Y f := (∏) i, smap (F i) f;
+  |})
+  (fun I F G f => {|
+    strans X := ∏' i, f i X;
+  |})
+  (fun I F i => {|
+    strans X := @π C I (fun i => F i X) i;
+  |})
+  _.
+Next Obligation.
+  setoid_rewrite smap_id.
+  apply spmap_id.
+Qed.
+Next Obligation.
+  setoid_rewrite smap_comp at 1.
+  apply spmap_comp.
+Qed.
+Next Obligation.
+  rename I0 into J.
+  assert (forall i (j1 j2: J), smap (F i) π₁ ∘ π j1 ∘ ((∏) j, π i ∘ e) = smap (F i) π₂ ∘ π j2 ∘ ((∏) j, π i ∘ e)).
+  1: {
+    intros i j1 j2.
+    specialize (H j1 j2).
+    rewrite to_sprod_eq in H.
+    specialize (H i).
+    rewrite !comp_assoc in H.
+    setoid_rewrite (pi_spmap _ i) in H.
+    rewrite !comp_assoc.
+    rewrite <- !(comp_assoc (smap (F i) _)).
+    setoid_rewrite (pi_spmap _ j1).
+    setoid_rewrite (pi_spmap _ j2).
+    now rewrite !comp_assoc.
+  }
+  specialize (fun i => smap_cover (F i) _ (H0 i)).
+  clear H H0.
+  intros Hu.
+  apply ex_unique_forall in Hu.
+  destruct Hu as [u [Hu H1]].
+  exists (sfork u); split.
+  apply to_sprod_eq.
+  intros j.
+  rewrite comp_assoc.
+  setoid_rewrite (pi_sfork _ j).
+  rewrite spmap_sfork.
+  apply to_sprod_eq.
+  intros i.
+  rewrite comp_assoc.
+  setoid_rewrite (pi_sfork _ i).
+  specialize (Hu i).
+  rewrite to_sprod_eq in Hu.
+  specialize (Hu j).
+  rewrite !comp_assoc in Hu.
+  setoid_rewrite (pi_sfork _ j) in Hu.
+  exact Hu.
+  intros g He.
+  clear Hu.
+  subst e.
+  apply to_sprod_eq.
+  intros i.
+  rewrite (pi_sfork u).
+  enough (u = fun i => π i ∘ g).
+  now apply (f_equal (fun f => f i)) in H.
+  clear i.
+  apply H1; clear H1.
+  intros i.
+  rewrite !comp_assoc.
+  f_equal.
+  rewrite spmap_sfork.
+  apply to_sprod_eq.
+  intros j.
+  rewrite !comp_assoc.
+  setoid_rewrite (pi_sfork _ j).
+  symmetry.
+  exact (pi_spmap (fun i => smap (F i) (ι j)) i).
+Qed.
+Next Obligation.
+  rewrite spmap_sfork.
+  apply to_sprod_eq.
+  intros i.
+  rewrite comp_assoc.
+  setoid_rewrite (pi_sfork _ i).
+  apply snatural.
+Qed.
+Next Obligation.
+  exact (pi_spmap (fun i => smap (F i) f) i).
+Qed.
+Next Obligation.
+  setoid_rewrite hom_eq; simpl.
+  now setoid_rewrite sfork_ump.
+Qed.
+Canonical sprodCat (T: Topology) (C: SProdCategory) := SProdCategory.Pack (cat T C) (sprodCat_mixin T C).
 
 End Sheaf.
 
